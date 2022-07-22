95aef63c2aa981a6b7c8d2ec04a9167a97257b0b
Vera Kirayonak <fogel_ver@mail.ru>
2022-07-23 00:55:45 +0300

    feat: add mobile slider and pagination behaviour

--- travel/slider.js
+++ travel/slider.js
@@ -1,8 +1,12 @@
 let slider = document.querySelector('.slider-list');
 let sliderItems = document.querySelectorAll('.slider-item');
 let paginationCircles = document.querySelectorAll('.pagination-circle');
+let leftControl = document.querySelector('.left-arrow');
+let rightControl = document.querySelector('.right-arrow');
+let paginationCirclesMobile = document.querySelectorAll('.pagination-circle-mobile');
 
-sliderItems.forEach((sliderItem) => {
+if (window.innerWidth > 391) {
+  sliderItems.forEach((sliderItem) => {
   sliderItem.onclick = function () {
     if (sliderItem.classList.contains('left-item')) {
       slider.classList.add('to-right');
@@ -29,8 +33,83 @@ sliderItems.forEach((sliderItem) => {
     }
   }
 })
+}
 
-// if (window.innerWidth < 391)
+
+const mobileToLeft = () => {
+  slider.style.transform = `translateX(calc((-860/2520*100%))`;
+}
+
+const mobileToLeftDouble = () => {
+  slider.style.transform = `translateX(calc(2*(-860/2520*100%)))`;
+}
+
+const mobileToRight = () => {
+  slider.style.transform = `translateX(calc((-860/2520*100%))`;
+}
+
+const mobileToRightDouble = () => {
+  slider.style.transform = `translateX(0)`;
+}
+
+if (window.innerWidth <= 391) {
+  rightControl.onclick = function () {
+    if (document.querySelector('.active-item').classList.contains('moved-left')) {
+      rightControl.setAttribute('src', './assets/img/slider-arrow-right-inactive.svg');
+      paginationCirclesMobile[1].setAttribute('src', './assets/img/pagination.svg');
+      paginationCirclesMobile[2].setAttribute('src', './assets/img/pagination-active.svg');
+      mobileToLeftDouble();
+      document.querySelector('.active-item').classList.remove('moved-left');
+      document.querySelector('.active-item').classList.add('left-end');
+      } else if (document.querySelector('.active-item').classList.contains('rightend')) {
+        mobileToLeft();
+        document.querySelector('.active-item').classList.add('moved-left');
+        leftControl.setAttribute('src', './assets/img/slider-arrow-left-active.svg');
+        paginationCirclesMobile[0].setAttribute('src', './assets/img/pagination.svg');
+        paginationCirclesMobile[1].setAttribute('src', './assets/img/pagination-active.svg');
+        document.querySelector('.active-item').classList.remove('rightend');
+        } else if (document.querySelector('.active-item').classList.contains('moved-right')) {
+          mobileToLeftDouble();
+          rightControl.setAttribute('src', './assets/img/slider-arrow-right-inactive.svg');
+          paginationCirclesMobile[1].setAttribute('src', './assets/img/pagination.svg');
+          paginationCirclesMobile[2].setAttribute('src', './assets/img/pagination-active.svg');
+          document.querySelector('.active-item').classList.remove('moved-right');
+          document.querySelector('.active-item').classList.add('left-end');
+          } else if (document.querySelector('.active-item').classList.contains('left-end')) {
+            return;
+            }
+    
+  }
+  leftControl.onclick = function () {
+    if (document.querySelector('.active-item').classList.contains('left-end')) {
+      mobileToRight();
+      document.querySelector('.active-item').classList.add('moved-right');
+      leftControl.setAttribute('src', './assets/img/slider-arrow-left-active.svg');
+      rightControl.setAttribute('src', './assets/img/slider-arrow-right.svg');
+      paginationCirclesMobile[1].setAttribute('src', './assets/img/pagination-active.svg');
+      paginationCirclesMobile[0].setAttribute('src', './assets/img/pagination.svg');
+      paginationCirclesMobile[2].setAttribute('src', './assets/img/pagination.svg');
+      document.querySelector('.active-item').classList.remove('left-end');
+    } else if (document.querySelector('.active-item').classList.contains('moved-right')) {
+      mobileToRightDouble();
+      leftControl.setAttribute('src', './assets/img/slider-arrow-left.svg');
+      rightControl.setAttribute('src', './assets/img/slider-arrow-right.svg');
+      paginationCirclesMobile[0].setAttribute('src', './assets/img/pagination-active.svg');
+      paginationCirclesMobile[1].setAttribute('src', './assets/img/pagination.svg');
+      paginationCirclesMobile[2].setAttribute('src', './assets/img/pagination.svg');
+      document.querySelector('.active-item').classList.remove('moved-right');
+      document.querySelector('.active-item').classList.add('rightend');
+    } else if (document.querySelector('.active-item').classList.contains('moved-left')) {
+      mobileToRightDouble();
+      leftControl.setAttribute('src', './assets/img/slider-arrow-left.svg');
+      paginationCirclesMobile[0].setAttribute('src', './assets/img/pagination-active.svg');
+      paginationCirclesMobile[1].setAttribute('src', './assets/img/pagination.svg');
+      paginationCirclesMobile[2].setAttribute('src', './assets/img/pagination.svg');
+      document.querySelector('.active-item').classList.remove('moved-left');
+      document.querySelector('.active-item').classList.add('rightend');
+    }
+  }
+}
 
 // function toLeft() {
 //   // document.querySelector('.left-item'). classList.remove('left-item');
